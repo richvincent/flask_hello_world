f47c9c42027785f1bfaafe4860298231702816b8
Richard Vincent <rich@richardvincent.com>
2015-12-22 20:19:03 +0000

    Jedi Remix

--- hello_world.py
+++ hello_world.py
@@ -12,7 +12,33 @@ def say_hi():
 
 @app.route("/hello/<name>")
 def hi_person(name):
-    return "Hello {}!".format(name.title())
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten. Awww...
+        </p>
+        <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150">
+    """
+    return html.format(name.title())
+
+
+@app.route("/jedi/<firstName>/<lastName>")
+def jedi_name(firstName, lastName):
+    #
+    jediName = lastName[0:3] + firstName[0:2]
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten. Awww...
+        </p>
+        <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150">
+    """
+    return html.format(jediName.title())
+
 
 if __name__ == "__main__":
     app.run(host=environ['IP'],
